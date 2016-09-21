7e0216d3e01071b070de94ef691e593571353607
pmock2 <payton.mock@gmail.com>
2016-09-21 16:02:00 -0400

    CPIU uploaded
    Payton Mock CPU.java uploaded

--- VirutalOperatingSystem/src/CPU.java
+++ VirutalOperatingSystem/src/CPU.java
@@ -1,4 +1,32 @@
+package operatingSystem;
 
 public class CPU {
-
+	
+	static void contextSwitchIn()
+	{
+		/**
+		 * Context Switch In
+		 * Array
+		 *  0. Idle
+		 *  1. terminate
+		 *  2. ready
+		 *  3. wait
+		 *  4. interrupt
+		 */
+		boolean stateArray[] = {true, false, false, false, false};
+	}
+	
+	static void contextSwitchOut()
+	{
+		/**
+		 * Out
+		 */
+	}
+	
+	static void run()
+	{
+	/**
+	 * run function	
+	 */
+	}
 }
