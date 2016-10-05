c87cf1bf6f43bbf74e38d9fd376344c420fe995e
Elizabeth-Herndon <meherndon32@gmail.com>
2016-10-05 15:10:47 -0400

    HardDisk change

--- VirutalOperatingSystem/src/HardDisk.java
+++ VirutalOperatingSystem/src/HardDisk.java
@@ -1,4 +1,20 @@
 
+//HDD contains an array of strings and a list of PCBs
 public class HardDisk {
-
+//Read the text file
+	//String X;
+	//x = fin.readLine();
+	/*while (x != null){
+	 * parse current line;
+	 * JOB 1 17 2
+	 * string [] = 
+	 * initialize job header
+	 * Job length = 17;
+	 * for(Job length)
+	 * {
+	 * 		read line and add to HDD list;
+	 * }
+	 * read next job header;
+	 * }
+	 */
 }
