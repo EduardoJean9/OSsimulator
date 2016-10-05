70185ec9a59040d51162a3deb9df2312eac78afc
pmock2 <payton.mock@gmail.com>
2016-10-05 16:19:44 -0400

    Method

--- VirutalOperatingSystem/src/HardDisk.java
+++ VirutalOperatingSystem/src/HardDisk.java
@@ -1,39 +1,37 @@
 import java.util.*;
 import java.io.*;
 //HDD contains an array of strings and a list of PCBs
-public class HardDisk {
+
+public class HardDisk 
+{
 	
-	String filename = "Program-File.txt";
-	String line = null;
-	StringBuffer stringbuf = new StringBuffer();
-	try{
-		FileReader filereader = new FileReader(filename);
-		BufferedReader bufferedReader = new BufferedReader(filereader);
-		while((line = bufferedReader.readLine()) != null){
-			stringbuf.append(line).append("\n");
+	static void input()
+	{
+		String filename = "Program-File.txt";
+		String line = null;
+		
+		StringBuffer stringbuf = new StringBuffer();
+		
+		try
+		{
+			FileReader filereader = new FileReader(filename);
+			BufferedReader bufferedReader = new BufferedReader(filereader);
+			while((line = bufferedReader.readLine()) != null)
+			{
+				stringbuf.append(line).append("\n");
+			}
+			bufferedReader.close();
 		}
-		bufferedReader.close();
+		catch(FileNotFoundException ex1)
+		{
+			System.out.println("Error: File was not found.");
+		}
+		catch(IOException ex)
+		{
+			System.out.println("Error: IO");
+		}
 	}
-	catch(FileNotFoundException ex1){
-		System.out.println("Error: File was not found.");
-	}
-	catch(IOException ex){
-		System.out.println("Error: IO");
-	}
-//Read the text file
-	//String X;
-	//x = fin.readLine();
-	/*while (x != null){
-	 * parse current line;
-	 * JOB 1 17 2
-	 * string [] = 
-	 * initialize job header
-	 * Job length = 17;
-	 * for(Job length)
-	 * {
-	 * 		read line and add to HDD list;
-	 * }
-	 * read next job header;
-	 * }
-	 */
-}}
+}
+
+
+
