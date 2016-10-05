7749b4e4ef2566c5dde997a22d2fb742d6c77560
Eduardo Jean <eduardojean9@gmail.com>
2016-10-05 16:03:12 -0400

    Buffered Reader
    Added a reader to read the program file

--- VirutalOperatingSystem/src/HardDisk.java
+++ VirutalOperatingSystem/src/HardDisk.java
@@ -1,6 +1,25 @@
-
+import java.util.*;
+import java.io.*;
 //HDD contains an array of strings and a list of PCBs
 public class HardDisk {
+	
+	String filename = "Program-File.txt";
+	String line = null;
+	StringBuffer stringbuf = new StringBuffer();
+	try{
+		FileReader filereader = new FileReader(filename);
+		BufferedReader bufferedReader = new BufferedReader(filereader);
+		while((line = bufferedReader.readLine()) != null){
+			stringbuf.append(line).append("\n");
+		}
+		bufferedReader.close();
+	}
+	catch(FileNotFoundException ex1){
+		System.out.println("Error: File was not found.");
+	}
+	catch(IOException ex){
+		System.out.println("Error: IO");
+	}
 //Read the text file
 	//String X;
 	//x = fin.readLine();
@@ -17,4 +36,4 @@ public class HardDisk {
 	 * read next job header;
 	 * }
 	 */
-}
+}}
