bb5040e3c23adb451805086f340e68b816b7c3ac
Elizabeth-Herndon <meherndon32@gmail.com>
2016-10-10 15:21:25 -0400

    OS Psudocode

--- VirutalOperatingSystem/src/OS.java
+++ VirutalOperatingSystem/src/OS.java
@@ -1,4 +1,32 @@
+import java.util.*;
+import java.io.*;
 
 public class OS {
+	//Ready queue
+		//Waiting queue
+		//Suspended queue
+		
+		public static void longTermSchedular()
+		{
+			//get the size of the next job from HD
+			
+			//ask the RAM if it has room
+		}
+		
+		public static void shortTermSchedular()
+		{
+			//FIFO or Priority
+			/*if FIFO
+			 * {
+			 * 		take the first job and load it into RAM
+			 * }
+			 * 
+			 * else if Priority
+			 * {
+			 * 		seach the list of jobs and locate the shorest job
+			 * }
+			 */
+		}
+
 	
 }
