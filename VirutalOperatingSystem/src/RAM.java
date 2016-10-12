515fbc6b23a1f3d3f0e396ed5c0878de4af18a61
pmock2 <payton.mock@gmail.com>
2016-10-12 15:39:01 -0400

    ram fxes

--- VirutalOperatingSystem/src/RAM.java
+++ VirutalOperatingSystem/src/RAM.java
@@ -1,15 +1,43 @@
+import java.util.ArrayList;
 
 public class RAM {
 	
-	static string getRAM()
+	public static ArrayList<ArrayList<String>> pageTable = new ArrayList<ArrayList<String>>();
+	public static ArrayList<String> ramData = new ArrayList<String>();
+	
+	//go to memory location and return data
+	public static String getRAM(int location)
 	{
-		
+			return (ramData.get(location));
 	}
 	
-	static storeRAM(string in)
+	//finds memory location and sets value to NULL
+	public static void eraseRAM(int location)
 	{
-		
+		ramData.remove(location);
 	}
 	
-	string data [4][8];
+	//set every memory location in RAM to NULL
+	public static void clearRAM(int location)
+	{
+		for(int i=0;i<31;i++)
+		{
+			ramData.remove(location);
+		}
+	}
+	
+	//finds empty memory location and stores 'in'.  Returns memory location.
+	public static int storeRAM(String in)
+	{
+		for(int i=0;i<31;i++)
+		{
+			if(ramData.get(i) == null)
+			{
+				ramData.set(i, in);
+				return i;
+			}
+		}
+		return -1;
+	}
+	
 }
