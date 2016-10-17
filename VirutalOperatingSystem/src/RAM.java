cb6d7fe8152b8097dd47c4c8dc6ae500ba211f30
Elizabeth-Herndon <meherndon32@gmail.com>
2016-10-17 14:43:20 -0400

    Update RAM

--- VirutalOperatingSystem/src/RAM.java
+++ VirutalOperatingSystem/src/RAM.java
@@ -1,29 +1,51 @@
-import java.util.ArrayList;
 
 public class RAM {
 	
-	public static ArrayList<ArrayList<String>> pageTable = new ArrayList<ArrayList<String>>();
-	public static ArrayList<String> ramData = new ArrayList<String>();
+	public int[][] pageTable; //Column 0 Job Num, Column 1 Page Num
+	public String[] ramData;
+	public int numpages;
+	public int pagesize;
+	
+	public RAM(int numpages, int pagesize)
+	{
+		pageTable = new int[numpages][2];
+		ramData = new String[numpages * pagesize];
+		this.numpages = numpages;
+		this.pagesize = pagesize;
+		for(int i = 0; i < numpages, i++)
+		{
+			pageTable[i][0] = -1;
+			pageTable[i][1] = i;
+		}
+	}
 	
 	//go to memory location and return data
-	public static String getRAM(int location)
+	public String getRAM(int jobnum, int virtualAddress)
 	{
-			return (ramData.get(location));
-	}
-	
-	//finds memory location and sets value to NULL
-	public static void eraseRAM(int location)
-	{
-		ramData.remove(location);
-	}
-	
-	//set every memory location in RAM to NULL
-	public static void clearRAM(int location)
-	{
-		for(int i=0;i<31;i++)
+		int virtualPageNum = (virtualAddress / pagesize) + 1;
+		int physicalPageNum = -1;
+		int counter = 0;
+		for(int i = 0; i < numpages; i++)
 		{
-			ramData.remove(location);
+			if(pageTable[i][0] == jobnum)
+			{
+				counter++;
+				if(virtualPageNum == counter) //set physical page number
+				{
+					physicalPageNum = pageTable[i][1];
+					break;
+				}
+			}
 		}
+		
+		int offSet = virtualAddress % pagesize;
+		
+		return ramData[(physicalPageNum * pagesize) + offSet];
+	}
+	
+	public boolean canFit(PCB in)
+	{
+		
 	}
 	
 	//finds empty memory location and stores 'in'.  Returns memory location.
