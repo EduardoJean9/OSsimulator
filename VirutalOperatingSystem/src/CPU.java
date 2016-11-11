257e539c13afe18e51cd511fc8272b6fd82aa61c
pmock2 <payton.mock@gmail.com>
2016-11-11 16:04:55 -0500

    CPU stuff

--- VirutalOperatingSystem/src/CPU.java
+++ VirutalOperatingSystem/src/CPU.java
@@ -24,13 +24,14 @@ public class CPU {
 	 *  5. interrupt
 	 */
 	boolean stateArray[] = {true, false, false, false, false, false};
+	
 	/*
 	 * register array
 	 * register 0 is the accumulator
 	 * register 1 is always set to 0 
 	 */
-	
 	int register[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	
 	public void contextSwitchIn(PCB in)
 	{
 		this.jobNumber = in.getJobNumber();;
@@ -65,7 +66,14 @@ public class CPU {
 			
 			if (instructionBin.equals("00")) //Arithmetic instruction format
 			{
-				
+				//add, subtract, multiply, divide (based on opcode)
+				//first two bits are not used
+				//next 6 bits are opcode for (+,-,*,/)
+				//next four bits are the first part of solution (think x)
+				//next four bits are the second part (think y)
+				//next four bits are the answer to the question
+				//example (x+y=z)
+				//last 12 bits are always 0
 			}
 			
 			else if (instructionBin.equals("01")) //Conditional Branch and Immediate format
@@ -75,7 +83,7 @@ public class CPU {
 			
 			else if (instructionBin.equals("10")) //Unconditional Jump format
 			{
-				
+				//jump to address (last 16 bits)
 			}
 			
 			else if (instructionBin.equals("11")) //Input and Output instruction format
@@ -90,31 +98,45 @@ public class CPU {
 				int reg2Conv = Integer.parseInt(reg1, 2);
 				reg1 = Integer.toString(reg1Conv, 10);
 				reg2 = Integer.toString(reg2Conv, 10);
+				reg1Conv = Integer.parseInt(reg1);
+				reg2Conv = Integer.parseInt(reg2);
 				
 				String address = instructionBin.substring(16, 32); //convert address to decimal
 				int addressConv = Integer.parseInt(address, 2);
 				address = Integer.toString(addressConv, 10);
-				
+				addressConv = Integer.parseInt(address);
 				switch (opcode)
 				//opcode for 11 can only rd or wr
 				{
 					case "0": //read
 						if (reg2.equals("0"))
 						{
-							//use the address
+							register[reg1Conv-1] = addressConv; 
 						}
 						else if (!reg2.equals("0"))
 						{
-							//use reg2
+							register[reg1Conv-1] = reg2Conv;
 						}
 						else
 						{
 							//error in reading reg2
-							System.out.println("Error in reading instruction: register2 not found.");
+							System.out.println("Error in reading I/O instruction: register2 not found.");
 						}
 						break;
 					case "1": //write
-						
+						if (reg2.equals("0"))
+						{
+							 
+						}
+						else if (!reg2.equals("0"))
+						{
+							
+						}
+						else
+						{
+							//error in reading reg2
+							System.out.println("Error in reading I/O instruction: register2 not found.");
+						}
 						break;
 				}
 			}
