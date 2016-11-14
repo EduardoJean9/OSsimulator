fb3dc3fcff367dce23df14f1f5ec03a1b78c6b00
pmock2 <payton.mock@gmail.com>
2016-11-14 16:37:32 -0500

    LOTS of CPU stuff

--- VirutalOperatingSystem/src/CPU.java
+++ VirutalOperatingSystem/src/CPU.java
@@ -17,13 +17,12 @@ public class CPU {
 	/*
 	 *  Flag Array
 	 *  0. Idle
-	 *  1. Send to IO queue
-	 *  2. send to wait queue
-	 *  3. send to terminate
-	 *  4. send to ready queue
-	 *  5. interrupt
+	 *  1. send to wait queue
+	 *  2. send to terminate
+	 *  3. send to ready queue
+	 *  4. Interrupt
 	 */
-	boolean stateArray[] = {true, false, false, false, false, false};
+	boolean stateArray[] = {true, false, false, false, false};
 	
 	/*
 	 * register array
@@ -58,7 +57,8 @@ public class CPU {
 	
 	public void run()
 	{
-		while (!stateArray[5])
+		stateArray[0] = false;
+		while (!stateArray[4])
 		{
 			String instruction = ram.getRAM(jobNumber, programCounter);
 			String format = hexToBinary(instruction);
@@ -75,7 +75,7 @@ public class CPU {
 				//next four bits are the first part of solution (think x)
 				//next four bits are the second part (think y)
 				//next four bits are the answer to the question
-				String reg1 = instructionBin.substring(8, 12); //convert reg 1 and reg 2 to decimal
+				String reg1 = instructionBin.substring(8, 12); //convert reg 1 and reg 2 and reg 3 to decimal
 				String reg2 = instructionBin.substring(12, 16);
 				String reg3 = instructionBin.substring(16, 20);
 				int reg1Conv = Integer.parseInt(reg1, 2);
@@ -91,16 +91,16 @@ public class CPU {
 				switch(opcode)
 				{
 					case "5": //add
-						register [reg3Conv-1] = register[reg1Conv-1] + register[reg2Conv-1];
+						register[reg1Conv] = register[reg2Conv] + register[reg3Conv];
 						break;
 					case "6": //sub
-						register[reg3Conv-1] = register[reg1Conv-1] - register[reg2Conv-1];
+						register[reg3Conv] = register[reg1Conv] - register[reg2Conv];
 						break;
 					case "7": //mult
-						register[reg3Conv-1] = register[reg1Conv-1] * register[reg2Conv-1];
+						register[reg3Conv] = register[reg1Conv] * register[reg2Conv];
 						break;
 					case "8": //div
-						register[reg3Conv-1] = register[reg1Conv-1] / register[reg2Conv-1];
+						register[reg3Conv] = register[reg1Conv] / register[reg2Conv];
 						break;
 				}
 				
@@ -110,12 +110,59 @@ public class CPU {
 			
 			else if (instructionBin.equals("01")) //Conditional Branch and Immediate format
 			{
+				String opcode = instructionBin.substring(2, 8); //convert opcode to hex
+				int opcodeConv = Integer.parseInt(opcode, 2);
+				opcode = Integer.toString(opcodeConv, 16);
 				
+				String reg1 = instructionBin.substring(8, 12); //convert reg 1 and reg 2 to decimal
+				String reg2 = instructionBin.substring(12, 16);
+				int reg1Conv = Integer.parseInt(reg1, 2);
+				int reg2Conv = Integer.parseInt(reg2, 2);
+				reg1 = Integer.toString(reg1Conv, 10);
+				reg2 = Integer.toString(reg2Conv, 10);
+				reg1Conv = Integer.parseInt(reg1);
+				reg2Conv = Integer.parseInt(reg2);
+				
+				String address = instructionBin.substring(16, 32); //convert address to decimal
+				int addressConv = Integer.parseInt(address, 2);
+				address = Integer.toString(addressConv, 10);
+				addressConv = Integer.parseInt(address);
+				switch (opcode)
+				{
+					case "21": //reg1 == reg2
+						break;
+					case "22": //reg1!=reg2
+						break;
+					case "23": //reg1 == 0
+						break;
+					case "24": //reg1 != 0
+						break;
+					case "25": //reg1 > 0
+						break;
+					case "26": //reg1 < 0
+						break;
+				}
 			}
 			
 			else if (instructionBin.equals("10")) //Unconditional Jump format
 			{
-				//jump to address (last 16 bits)
+				String opcode = instructionBin.substring(2, 8); //convert opcode to hex
+				int opcodeConv = Integer.parseInt(opcode, 2);
+				opcode = Integer.toString(opcodeConv, 16);
+				
+				String address = instructionBin.substring(9, 32); //convert address to decimal
+				int addressConv = Integer.parseInt(address, 2);
+				address = Integer.toString(addressConv, 10);
+				addressConv = Integer.parseInt(address);
+				if (opcodeConv == 20)
+					this.jobNumber = addressConv;
+				else if (opcodeConv == 18)
+				{
+					stateArray[4] = true;
+					stateArray[0] = true;
+				}
+				else
+					System.out.println("Error in reading opcode for unonditional jump.");
 			}
 			
 			else if (instructionBin.equals("11")) //Input and Output instruction format
@@ -143,11 +190,11 @@ public class CPU {
 					case "0": //read
 						if (reg2.equals("0"))
 						{
-							register[reg1Conv-1] = addressConv; 
+							register[reg1Conv] = 0;//temp buffer at address 
 						}
 						else if (!reg2.equals("0"))
 						{
-							register[reg1Conv-1] = reg2Conv;
+							register[reg1Conv] = register[reg2Conv];
 						}
 						else
 						{
@@ -158,11 +205,11 @@ public class CPU {
 					case "1": //write
 						if (reg2.equals("0"))
 						{
-							 
+							 register[addressConv] = 0;//temp buffer at address
 						}
 						else if (!reg2.equals("0"))
 						{
-							register[reg2Conv-1] = reg1Conv;
+							register[reg2Conv] = register[reg1Conv];
 						}
 						else
 						{
